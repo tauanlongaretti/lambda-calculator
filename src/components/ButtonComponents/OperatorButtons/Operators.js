77001b5ddc7d7c45ba2863123cb8200442b946e2
tauan-longaretti <tauanlongaretti@gmail.com>
2019-11-15 23:56:05 -0500

    Completed steps one through four for Numbers and began Operators and Specials

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { operators } from '../../../data';
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+export const Operators = () => {
   // STEP 2 - add the imported data to state
   return (
     <div>
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { specials } from '../../../data';
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Specials = () => {
+export const Specials = () => {
   // STEP 2 - add the imported data to state
 
   return (
