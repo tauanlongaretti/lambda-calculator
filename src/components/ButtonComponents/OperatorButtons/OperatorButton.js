daebab3d47253719125ca575c820f1a3b11a09ca
tauan-longaretti <tauanlongaretti@gmail.com>
2019-11-18 21:07:37 -0500

    added styling

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -4,7 +4,7 @@ const OperatorButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-  <button className="{`${props.button.value}`}">{`${props.button.char}`}</button>
+  <button className="operators" value="{`${props.button.value}`}">{`${props.button.char}`}</button>
     </>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -4,7 +4,7 @@ const SpecialButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */} 
-      <button>{`${props.button}`}</button>    
+      <button className="specials">{`${props.button}`}</button>    
     </>
   );
 };
